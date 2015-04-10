640d9f3b49eed86da0a7f60abbe6c15218aeacf6
Tim Heuett <tim.heuett@gmail.com>
2015-04-09 19:38:31 -0700

    Dumping unused functions

--- js/app.js
+++ js/app.js
@@ -10,22 +10,6 @@ jQuery(function ($) {
 	var ESCAPE_KEY = 27;
 
 	var util = {
-		uuid: function () {
-			/*jshint bitwise:false */
-			var i, random;
-			var uuid = '';
-
-			for (i = 0; i < 32; i++) {
-				random = Math.random() * 16 | 0;
-				if (i === 8 || i === 12 || i === 16 || i === 20) {
-					uuid += '-';
-				}
-				uuid += (i === 12 ? 4 : (i === 16 ? (random & 3 | 8) : random)).toString(16);
-			}
-
-			return uuid;
-		},
-
 		getApiKey: function () {
 			var key = localStorage.getItem("github_key");
 			if (!key) {
@@ -37,15 +21,6 @@ jQuery(function ($) {
 
 		pluralize: function (count, word) {
 			return count === 1 ? word : word + 's';
-		},
-
-		store: function (namespace, data) {
-			if (arguments.length > 1) {
-				return localStorage.setItem(namespace, JSON.stringify(data));
-			} else {
-				var store = localStorage.getItem(namespace);
-				return (store && JSON.parse(store)) || [];
-			}
 		}
 	};
 
